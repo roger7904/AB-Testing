e68c66330fe9ddc687f4fd08d3786626bb931f63
roger <roger@25sprout.com>
2022-04-04 22:10:35 +0800

    remote config

--- app/src/main/java/com/roger/ab_testing/MainActivity.kt
+++ app/src/main/java/com/roger/ab_testing/MainActivity.kt
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.installations.FirebaseInstallations
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
@@ -21,10 +23,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        FirebaseInstallations.getInstance().getToken(/* forceRefresh */ true)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("Installations", "Installation auth token: " + task.result?.token)
+                } else {
+                    Log.e("Installations", "Unable to get Installation auth token")
+                }
+            }
         binding.run {
             // Obtain the FirebaseAnalytics instance.
             firebaseAnalytics = Firebase.analytics
-
+            firebaseAnalytics.setUserProperty("username", "test")
             val remoteConfig = Firebase.remoteConfig
             val configSettings = remoteConfigSettings {
                 minimumFetchIntervalInSeconds = if (BuildConfig.DEBUG) 0 else 3600
@@ -33,42 +43,13 @@ class MainActivity : AppCompatActivity() {
             remoteConfig.setDefaultsAsync(R.xml.remote_config_default) // 設定本地預設值
             remoteConfig.fetchAndActivate() // 使⽤值
                 .addOnSuccessListener {
-                    when (remoteConfig.getString("sign_up_type")) {
-                        "facebook" -> {
-                            clFacebook.visibility = View.VISIBLE
-                            clGoogle.visibility = View.GONE
-                        }
-                        "google" -> {
-                            clFacebook.visibility = View.GONE
-                            clGoogle.visibility = View.VISIBLE
+                    if (remoteConfig.getBoolean("new_feature_enabled")){
+                        btnNewFeature.setOnClickListener {
+                            Toast.makeText(baseContext, "New Feature",
+                                Toast.LENGTH_SHORT).show()
                         }
                     }
                 }
-
-            clFacebook.setOnClickListener {
-                firebaseAnalytics.logEvent("sign_up_btn") {
-                    param("sign_up","click")
-                }
-            }
-            clGoogle.setOnClickListener {
-                firebaseAnalytics.logEvent("sign_up_btn") {
-                    param("sign_up","click")
-                }
-            }
-
-
-//            btnSendEvent.setOnClickListener {
-//                Log.d("TAG", "onCreate: ")
-//                firebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN) {
-//                    param(FirebaseAnalytics.Param.METHOD, "test")
-//                }
-//                firebaseAnalytics.logEvent("test_event_name") {
-//                    param("event_category","event_action")
-//                }
-//                firebaseAnalytics.logEvent("sign_up_btn") {
-//                    param("sign_up","click")
-//                }
-//            }
         }
     }
 }
